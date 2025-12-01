4dbdb9848e2a0a0cdd7dffe506270cb62bae3252
EmmanouilKoutrokois <emmanouilkoutrokois@gmail.com>
2025-12-01 11:28:29 +0200

    Initial commit

--- src/main/java/com/example/employeeManagement/controller/CompanyController.java
+++ src/main/java/com/example/employeeManagement/controller/CompanyController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/companies")
+@RequestMapping("/companies")
 public class CompanyController {
 
     private final CompanyServiceImpl companyServiceImpl;
