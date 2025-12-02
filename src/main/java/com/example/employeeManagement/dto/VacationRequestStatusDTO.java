d80dce3ddd24da4ddecd6b4ec8495b113668c7dc
EmmanouilKoutrokois <emmanouilkoutrokois@gmail.com>
2025-12-02 11:58:43 +0200

    Initial commit

---
+++ src/main/java/com/example/employeeManagement/dto/VacationRequestStatusDTO.java
@@ -0,0 +1,9 @@
+package com.example.employeeManagement.dto;
+
+import lombok.Data;
+
+@Data
+public class VacationRequestStatusDTO {
+    private Long vacationId;
+    private String status;
+}
--- src/main/java/com/example/employeeManagement/vacationRequestStatus/vacationRequestStatusDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeeManagement.vacationRequestStatus;
-
-import lombok.Data;
-
-@Data
-public class vacationRequestStatusDTO {
-    private Long vacationId;
-    private String status;
-}
